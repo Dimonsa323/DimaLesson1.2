a64d63d70837a570e5e3f1c3f3b051eebc8375db
Dima Hubenya <dima96gubenya@gmail.com>
2022-06-17 10:16:27 +0300

    Initial Commit

---
+++ DimaLesson1.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DimaLesson1.1
+//
+//  Created by Дима Губеня on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var helloWorldLable: UIView!
+    
+    @IBOutlet weak var showTextLabel: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showTextButton() {
+    }
+    
+}
+
+
